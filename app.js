c38ea9575a4ce2cb11d6b73ed84c3c778a8df4e9
Cuber00 <snidak2018@yandex.ru>
2022-05-27 21:47:17 +0700

    Закончил основную часть ДЗ

--- app.js
+++ app.js
@@ -5,17 +5,41 @@ const goods = [
     { title: 'Shoes', price: 250 },
 ];
 
-const renderGoodsItem = (title, price) => 
-    `
-        <div class="goods-item">
-            <h3>${title}</h3>
-            <p>${price}</p>
-        </div>
-    `;
 
 
-const renderGoodsList = (list = []) => {
-    let goodsList = list.map(({title, price}) => renderGoodsItem(title, price));
-    document.querySelector('.goods-list').innerHTML = goodsList.join(" ");
+class GoodsItem {
+    constructor({title = "", price = 0}){
+        this.title = title;
+        this.price = price;
+    }
+    render() {
+        return `
+            <div class="goods-item">
+                <h3>${this.title}</h3>
+                <p>${this.price}</p>
+            </div>
+        `
+    }
 }
-renderGoodsList(goods);
+
+class GoodsList{
+    fetchData(){
+        this.list = goods;
+    }
+    getCount(){
+        return this.list.reduce((sum, item) => sum + item.price, 0)
+    }
+    render(){
+        const goodList = this.list.map(item => {
+            const goodsItem = new GoodsItem(item);
+            return goodsItem.render();
+        }).join(" ");
+        document.querySelector('.goods-list').innerHTML = goodList;
+    }
+}
+
+const goodsList = new GoodsList()
+
+goodsList.fetchData();
+goodsList.render();
+console.log(goodsList.getCount())
